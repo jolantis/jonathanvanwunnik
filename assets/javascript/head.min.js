22deb55aea0a2791d7690cf022d19e540f327c50
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-12-21 13:47:17 +0100

    Build v1.0.10.

--- assets/javascript/head.min.js
+++ assets/javascript/head.min.js
@@ -1,5 +1,5 @@
 /**
- * Portfolio of UX/UI Designer Jonathan van wunnik — v1.0.9 — 2018-12-21
+ * Portfolio of UX/UI Designer Jonathan van wunnik — v1.0.10 — 2018-12-21
  * https://artlantis.nl
  * Copyright (c) 2018 Jonathan van Wunnik
  * License MIT
